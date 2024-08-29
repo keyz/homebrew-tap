9a27bf29ab5f7849552840c1dee0b39503eed1f2
Keyan Zhang <2268452+keyz@users.noreply.github.com>
2024-08-29 02:06:13 -0700

    comicsans 0.4.0

--- Formula/comicsans.rb
+++ Formula/comicsans.rb
@@ -1,8 +1,8 @@
 class Comicsans < Formula
   desc "cs (comic sans) for :pink-slack-emoji:"
   homepage "https://github.com/keyz/comicsans"
-  url "https://github.com/keyz/comicsans/releases/download/0.3.0/macOS-universal.tar.gz"
-  sha256 "e6c778a57edeb0917594e7a78c986687a108ffc444f540a08de41803c815be48"
+  url "https://github.com/keyz/comicsans/releases/download/0.4.0/macOS-universal.tar.gz"
+  sha256 "eb5a10e65a744efaece6cca6a5f902c04e81fe9a461ba6b8c42f7019fcb5b4dd"
   license "MIT"
 
   depends_on :macos # macOS only
